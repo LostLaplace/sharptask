42153f93de3ce94961ef198ab5609cc1c6cb413d
Cameron Johnson <cam.j.johnson@icloud.com>
2025-04-19 18:07:09 -0500

    Move all config logic into config module
    Separate config logic into dedicated module.

--- src/config.rs
+++ src/config.rs
@@ -1,53 +1,100 @@
-use std::path::PathBuf;
+use std::{path::PathBuf, str::FromStr};
 use std::fs;
+use chrono_tz::Tz;
 use serde::Deserialize;
-use anyhow::{anyhow, Result, Context};
+use anyhow::{anyhow, Result};
 use std::path::Path;
+use clap::Parser;
+
+#[derive(Debug)]
+pub struct Config {
+    pub vault_path: PathBuf,
+    pub task_path: PathBuf,
+    pub timezone: Tz
+}
 
 const DEFAULT_PATH: &str = "~/.sharptask/config.toml";
 
 #[derive(Deserialize)]
-pub struct Config {
-    pub vault_path: Option<PathBuf>,
-    pub task_path: Option<PathBuf>,
-    pub timezone: Option<String>
+struct ConfigFile {
+    vault_path: Option<PathBuf>,
+    task_path: Option<PathBuf>,
+    timezone: Option<String>
 }
 
-pub fn defaults() -> Config {
-    Config {
-        vault_path: None,
-        task_path: Some(PathBuf::from("~/.task")),
-        timezone: None
+impl Default for ConfigFile {
+    fn default() -> Self {
+        ConfigFile {
+            vault_path: None,
+            task_path: Some(PathBuf::from("~/.task")),
+            timezone: localzone::get_local_zone()
+        }
     }
 }
 
-pub fn parse<P: AsRef<Path>>(config_path: P) -> Result<Config> {
-    let contents = fs::read_to_string(&config_path)
-        .context(format!("Failed to read from file: {}", config_path.as_ref().display()))
-        .map_err(|e| anyhow!("Cannot read config file: {}", e))?;
-    let config: Config = toml::from_str(&contents)
-        .context(format!("Failed to parse TOML: \n{}", &contents))
-        .map_err(|_| anyhow!("Cannot parse TOML"))?;
-    Ok(config)
+
+#[derive(Parser, Debug)]
+#[command(version, about, long_about = None)]
+struct Cli {
+    #[arg(short, long)]
+    vault: Option<PathBuf>,
+    #[arg(short, long)]
+    task_db: Option<PathBuf>,
+    #[arg(short, long)]
+    config: Option<PathBuf>,
 }
 
-pub fn parse_from_default_path() -> Result<Config> {
-    let config_path = PathBuf::from(DEFAULT_PATH);
-    parse(&config_path)
+pub fn get() -> Config {
+    // First parse the CLI arguments
+    let cli = Cli::parse();
+
+    // Now try to parse the passed in config file if it exists or
+    // the default config file if not
+    let parsed_config = match cli.config {
+        Some(config_path) => parse(&config_path),
+        None => parse(DEFAULT_PATH)
+    }.unwrap_or(ConfigFile::default());
+
+    // The user can override a few of the options via CLI flags,
+    // ensure that these items are either defined in the config or 
+    // via CLI
+    let vault_path = cli.vault.or(parsed_config.vault_path)
+        .map(|path| {
+            let path_str = path.to_string_lossy();
+            let expanded = shellexpand::tilde(&path_str);
+            PathBuf::from(expanded.into_owned())
+        }).expect("Vault must be provided via CLI or config");
+
+    let task_path = cli.task_db.or(parsed_config.task_path)
+        .map(|path| {
+            let path_str = path.to_string_lossy();
+            let expanded = shellexpand::tilde(&path_str);
+            PathBuf::from(expanded.into_owned())
+        }).expect("Task DB path must be provided via --task_db or config");
+
+    let timezone = parsed_config.timezone.or(localzone::get_local_zone())
+        .expect("Cannot determine a timezone");
+
+    let tz = Tz::from_str(&timezone).expect(&format!("Cannot process timezone: {}", timezone));
+
+    Config {
+        vault_path,
+        task_path,
+        timezone: tz
+    }
+}
+
+fn parse<P: AsRef<Path>>(config_path: P) -> Result<ConfigFile> {
+    let contents = fs::read_to_string(&config_path)
+        .map_err(|e| anyhow!("Cannot read config file: {}", e))?;
+    let config: ConfigFile = toml::from_str(&contents)
+        .map_err(|_| anyhow!("Cannot parse TOML"))?;
+    Ok(config)
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
-    use std::path::PathBuf;
-
-    #[test]
-    fn parse_get_defaults() {
-        let my_config = defaults();
-        assert_eq!(my_config.vault_path, None);
-        assert_eq!(my_config.task_path.unwrap(), PathBuf::from("~/.task"));
-        assert_eq!(my_config.timezone, None);
-    }
 
     #[test]
     fn parse_simple_config() {
--- src/main.rs
+++ src/main.rs
@@ -1,51 +1,16 @@
 //use ignore::{types::TypesBuilder, WalkBuilder};
 //use grep::{regex::RegexMatcher, searcher::Searcher, searcher::sinks};
-use clap::Parser;
 //use taskchampion::{storage::AccessMode, Operations, Replica, Status, StorageConfig, Uuid};
-use std::path::PathBuf;
 //use regex::Regex;
 use colored::Colorize;
 use anyhow::{Result, anyhow};
 
 mod config;
 
-#[derive(Parser, Debug)]
-#[command(version, about, long_about = None)]
-struct Cli {
-    vault: Option<PathBuf>,
-    task_db: Option<PathBuf>,
-    config: Option<PathBuf>,
-}
-
-#[derive(Debug)]
-struct AppConfig {
-    vault_path: PathBuf,
-    task_path: PathBuf,
-    timezone: String
-}
-
 fn main() -> Result<()> {
-    let cli = Cli::parse();
+    let cfg = config::get(); 
 
-    let parsed_config = match cli.config {
-        Some(config_path) => config::parse(&config_path),
-        None => config::parse_from_default_path()
-    }.unwrap_or(config::defaults());
-
-    let vault_path = cli.vault.or(parsed_config.vault_path)
-        .ok_or_else(|| anyhow!("Vault must be provided via --vault or config".red()))?;
-    let task_path = cli.task_db.or(parsed_config.task_path)
-        .ok_or_else(|| anyhow!("Task DB path must be provided via --task_db or config".red()))?;
-    let timezone = parsed_config.timezone.or(localzone::get_local_zone())
-        .ok_or_else(|| anyhow!("Cannot determine a timezone"))?;
-
-    let my_config = AppConfig {
-        vault_path,
-        task_path,
-        timezone
-    };
-
-    println!("{:?}", my_config);
+    println!("{:?}", cfg);
 
     Ok(())
 
