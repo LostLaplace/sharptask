45f07f5c7578681d314b885e8f950aee72e07060
Cameron Johnson <cam.j.johnson@icloud.com>
2025-04-21 10:48:34 -0500

    Switch to grapheme cluster crate for better unicode support
    Adjust processing to return UUID as well as metadata

--- src/taskparser.rs
+++ src/taskparser.rs
@@ -2,7 +2,9 @@ use taskchampion::Uuid;
 use chrono::DateTime;
 use chrono_tz::Tz;
 use regex::Regex;
-use std::string::String;
+use std::{str::FromStr, string::String};
+use unicode_segmentation::UnicodeSegmentation;
+use anyhow::{Result, Context};
 
 #[derive(Debug, Eq, PartialEq)]
 pub enum Status {
@@ -20,22 +22,22 @@ pub enum Priority {
     Highest,
 }
 
-const SIGNIFICANT_EMOJI: &[&char] = &[
-    &'📅',
-    &'⏳',
-    &'🛫',
-    &'➕',
-    &'✅',
-    &'❌',
-    &'🔺',
-    &'⏫',
-    &'🔼',
-    &'🔽',
-    &'⏬',
-    &'🔁',
-    &'🆔',
-    &'⛔',
-    &'🔨',
+const SIGNIFICANT_EMOJI: &[&str] = &[
+    &"📅",
+    &"⏳",
+    &"🛫",
+    &"➕",
+    &"✅",
+    &"❌",
+    &"🔺",
+    &"⏫",
+    &"🔼",
+    &"🔽",
+    &"⏬",
+    &"🔁",
+    &"🆔",
+    &"⛔",
+    &"🔨",
 ];
 
 pub struct ObsidianTask {
@@ -62,21 +64,32 @@ pub fn parse<T: AsRef<str>>(task_string: T) -> Option<ObsidianTask> {
     None
 }
 
-fn parse_task_description(task: &mut String) -> Option<String> {
+fn extract_task_parts(task: &mut String) -> (Option<String>, Option<Result<Uuid>>) {
+    // Returns a tuple with the metadata string and UUID as options
+    let mut uuid: Option<Result<Uuid>> = None;
+    let uuid_re = Regex::new(r"(?<whole>\[\[uuid: (?<uuid>.*)\|\]\])").unwrap();
+    if let Some(caps) = uuid_re.captures(task) {
+        uuid = caps.name("uuid").map(|id| Uuid::parse_str(id.as_str()).with_context(|| format!("Failed to parse UUID: {}", id.as_str())));
+        if let Some(whole) = caps.name("whole") {
+            *task = task.replace(whole.as_str(), "").trim().to_string();
+        }
+    }
+
     // Capture up to first significant emoji, this is our task description with tags
     let mut task_desc = String::with_capacity(task.len());
     let mut metadata = None;
-    for sym in task.chars() {
-        if !SIGNIFICANT_EMOJI.contains(&&sym) {
-            task_desc.push(sym);
+    let graphemes = UnicodeSegmentation::graphemes(task.as_str(), true).collect::<Vec<&str>>();
+    for grapheme in graphemes {
+        if !SIGNIFICANT_EMOJI.contains(&grapheme) {
+            task_desc.push_str(grapheme);
         } else {
-            metadata = Some(task.replace(&task_desc, ""));
+            metadata = Some(task.replace(&task_desc, "").trim().to_string());
             break;
         }
     }
 
     *task = task_desc.trim().to_string();
-    metadata
+    (metadata, uuid)
 }
 
 fn parse_preamble(task_string: &mut String) -> Option<Status> {
@@ -100,41 +113,73 @@ mod tests {
     #[test]
     fn test_task_desc_trivial() {
         let mut trivial_case = String::from("");
-        let metadata = parse_task_description(&mut trivial_case);
+        let (metadata, uuid) = extract_task_parts(&mut trivial_case);
         assert!(metadata.is_none());
+        assert!(uuid.is_none());
         assert_eq!(trivial_case, "");
     }
 
     #[test]
     fn test_task_desc_simple() {
         let mut simple_task = String::from("This is some simple text");
-        let metadata = parse_task_description(&mut simple_task);
+        let (metadata, uuid) = extract_task_parts(&mut simple_task);
         assert!(metadata.is_none());
+        assert!(uuid.is_none());
         assert_eq!(simple_task, "This is some simple text");
     }
     
     #[test]
     fn test_task_desc_with_metadata() {
         let mut task = String::from("Task data that is 📅 2025-05-19");
-        let metadata = parse_task_description(&mut task);
+        let (metadata, uuid) = extract_task_parts(&mut task);
         assert_eq!(metadata.unwrap(), "📅 2025-05-19");
+        assert!(uuid.is_none());
         assert_eq!(task, "Task data that is");
     }
 
     #[test]
     fn test_task_desc_only_metadata() {
         let mut task = String::from("📅 2025-05-19");
-        let metadata = parse_task_description(&mut task);
+        let (metadata, uuid) = extract_task_parts(&mut task);
         assert_eq!(metadata.unwrap(), "📅 2025-05-19");
+        assert!(uuid.is_none());
         assert_eq!(task, "");
     }
 
     #[test]
     fn test_task_desc_emojis() {
         let mut task = String::from("Make a  🥪 📅 2025-05-19");
-        let metadata = parse_task_description(&mut task);
+        let (metadata, uuid) = extract_task_parts(&mut task);
         assert_eq!(metadata.unwrap(), "📅 2025-05-19");
+        assert!(uuid.is_none());
         assert_eq!(task, "Make a  🥪");
+    }
+
+    #[test]
+    fn test_uuid() {
+        let mut task = String::from("Test task stuff [[uuid: 96bb3816-aedd-4033-8ff6-4746a700aac8|]]");
+        let (metadata, uuid) = extract_task_parts(&mut task);
+        assert!(metadata.is_none());
+        assert_eq!(uuid.unwrap().unwrap(), Uuid::parse_str("96bb3816-aedd-4033-8ff6-4746a700aac8").unwrap());
+        assert_eq!(task, "Test task stuff");
+    }
+
+    #[test]
+    fn test_bad_uuid() {
+        let mut task = String::from("Test task stuff [[uuid: abcd|]]");
+        let (metadata, uuid) = extract_task_parts(&mut task);
+        assert!(metadata.is_none());
+        assert!(uuid.unwrap().is_err());
+        assert_eq!(task, "Test task stuff");
+    }
+
+    #[test]
+    fn test_metadata_and_uuid() {
+        let mut task = String::from("Test task stuff 📅 2025-05-19 [[uuid: 96bb3816-aedd-4033-8ff6-4746a700aac8|]]");
+        let (metadata, uuid) = extract_task_parts(&mut task);
+        assert_eq!(metadata.unwrap(), "📅 2025-05-19");
+        assert_eq!(uuid.unwrap().unwrap(), Uuid::parse_str("96bb3816-aedd-4033-8ff6-4746a700aac8").unwrap());
+        assert_eq!(task, "Test task stuff");
     }
 
     #[test]
